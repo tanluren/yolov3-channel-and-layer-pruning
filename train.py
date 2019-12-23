95bacbd3b5ea71b304ba0aff21a0ec2625b954e3
tanluren <1806380874@qq.com>
2019-12-23 16:17:04 +0800

    增加了蒸馏策略二

--- train.py
+++ train.py
@@ -286,7 +286,7 @@ def train():
     for epoch in range(start_epoch, epochs):  # epoch ------------------------------------------------------------------
         model.train()
         #print('learning rate:',optimizer.param_groups[0]['lr'])
-        print(('\n' + '%10s' * 9) % ('Epoch', 'gpu_mem', 'GIoU', 'obj', 'cls', 'total', 'soft', 'targets', 'img_size'))
+        print(('\n' + '%10s' * 10) % ('Epoch', 'gpu_mem', 'GIoU', 'obj', 'cls', 'total', 'soft', 'rratio', 'targets', 'img_size'))
 
         # Freeze backbone at epoch 0, unfreeze at epoch 1 (optional)
         freeze_backbone = False
@@ -355,13 +355,16 @@ def train():
                 return results
 
             soft_target = 0
+            reg_ratio = 0  #表示有多少target的回归是不如老师的，这时学生会跟gt再学习
             if t_cfg:
                 if mixed_precision:
                     with torch.no_grad():
                         output_t = t_model(imgs)
                 else:
                     _, output_t = t_model(imgs)
-                soft_target = distillation_loss1(pred, output_t, model.nc, imgs.size(0))
+                #soft_target = distillation_loss1(pred, output_t, model.nc, imgs.size(0))
+                #这里把蒸馏策略改为了二，想换回一的可以注释掉loss2，把loss1取消注释
+                soft_target, reg_ratio = distillation_loss2(model, targets, pred, output_t)
                 loss += soft_target
 
             # Scale loss by nominal batch_size of 64
@@ -385,8 +388,8 @@ def train():
             mloss = (mloss * i + loss_items) / (i + 1)  # update mean losses
             msoft_target = (msoft_target * i + soft_target) / (i + 1)
             mem = torch.cuda.memory_cached() / 1E9 if torch.cuda.is_available() else 0  # (GB)
-            s = ('%10s' * 2 + '%10.3g' * 7) % (
-                '%g/%g' % (epoch, epochs - 1), '%.3gG' % mem, *mloss, msoft_target,len(targets), img_size)
+            s = ('%10s' * 2 + '%10.3g' * 8) % (
+                '%g/%g' % (epoch, epochs - 1), '%.3gG' % mem, *mloss, msoft_target, reg_ratio, len(targets), img_size)
             pbar.set_description(s)
 
             # end batch ------------------------------------------------------------------------------------------------
